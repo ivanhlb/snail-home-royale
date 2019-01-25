37d9043db55bc67ea5902b6e55421d3ba8fd7aa5
cerealcatRepo <leonard_lsc@hotmail.com>
2019-01-26 04:19:13 +0800

    Initial

---
+++ Snail House Party/Assets/Scripts/SnailsPace/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SnailPace {
+
+    public class PlayerController : MonoBehaviour
+    {
+        int buttonMash = 0;
+        string prevButton;
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if(Input.GetKeyDown(KeyCode.A)){
+                buttonMash++;
+
+            }
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                buttonMash++;
+            }
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                buttonMash++;
+            }
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                buttonMash++;
+            }
+
+            if (buttonMash == 20){
+                transform.Translate(Vector3.right);
+                buttonMash = 0;
+            }
+
+        }
+    }
+}
