6d4f4156009e52a9397ac006435b9c1a10443fda
John Lu <johnlu456@hotmail.com>
2019-01-26 03:39:04 +0800

    Added gobbler script folder and FoodManager class

---
+++ Snail House Party/Assets/Scripts/Gobbler/FoodManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gobbler
+{
+    public class FoodManager : MonoBehaviour
+    {
+		
+
+        // Start is called before the first frame update
+        void Start ()
+        {
+			//Input.axis
+        }
+
+        // Update is called once per frame
+        void Update ()
+        {
+			
+        }
+
+		void GenerateFoodSequence ()
+		{
+			
+		}
+    }
+}
