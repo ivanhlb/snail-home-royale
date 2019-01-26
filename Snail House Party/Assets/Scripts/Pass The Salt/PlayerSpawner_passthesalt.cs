20bae885cba7d79b120ef70349bfb3c265161a88
RyouMisakl <33336762+RyouMisakl@users.noreply.github.com>
2019-01-26 14:22:24 +0800

    pass the salt bomb working

--- Snail House Party/Assets/Scripts/Pass The Salt/PlayerSpawner_passthesalt.cs
+++ Snail House Party/Assets/Scripts/Pass The Salt/PlayerSpawner_passthesalt.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class PlayerSpawner_passthesalt : MonoBehaviour
 {
@@ -13,30 +14,37 @@ public class PlayerSpawner_passthesalt : MonoBehaviour
     GameObject playerthreeprefab;
     [SerializeField]
     GameObject playerfourprefab;
+    public List<GameObject> playerplaying;
     // Start is called before the first frame update
     void Start()
     {
         gm = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+
+        if (gm.playerone)
+        {
+            GameObject playerone = Instantiate(playeroneprefab, playeroneprefab.transform.position, Quaternion.identity);
+            playerplaying.Add(playerone);
+        }
+        if (gm.playertwo)
+        {
+            GameObject playertwo = Instantiate(playertwoprefab, playertwoprefab.transform.position, Quaternion.identity);
+            playerplaying.Add(playertwo);
+        }
+        if (gm.playerthree)
+        {
+            GameObject playerthree = Instantiate(playerthreeprefab, playerthreeprefab.transform.position, Quaternion.identity);
+            playerplaying.Add(playerthree);
+        }
+        if (gm.playerfour)
+        {
+            GameObject playerfour = Instantiate(playerfourprefab, playerfourprefab.transform.position, Quaternion.identity);
+            playerplaying.Add(playerfour);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (gm.playerone)
-        {
-            Instantiate(playeroneprefab, playeroneprefab.transform);
-        }
-        if (gm.playertwo)
-        {
-            Instantiate(playertwoprefab, playertwoprefab.transform);
-        }
-        if (gm.playerthree)
-        {
-            Instantiate(playerthreeprefab, playerthreeprefab.transform);
-        }
-        if (gm.playerfour)
-        {
-            Instantiate(playerfourprefab, playerfourprefab.transform);
-        }
+        
     }
 }
---
+++ Snail House Party/Assets/Scripts/Pass The Salt/bomb.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class bomb : MonoBehaviour
+{
+    [SerializeField]
+    Text timer;
+    [SerializeField]
+    GameObject playercontroller;
+    PlayerSpawner_passthesalt controller;
+
+    public GameObject target;
+    public Double timetokill;
+    public float speed = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+       controller = playercontroller.GetComponent<PlayerSpawner_passthesalt>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+
+        if (timetokill <= 0)
+        {
+            if (target != null)
+            {
+                controller.playerplaying.Remove(target);
+                Destroy(target);
+            }
+            int randomtarget = UnityEngine.Random.Range(0, controller.playerplaying.Count - 1);
+            target = controller.playerplaying[randomtarget];
+            timetokill = 5;
+        }
+
+        if (target != null)
+        {
+            float step = speed * Time.deltaTime;
+            this.transform.position = Vector3.MoveTowards(this.transform.position, target.transform.position, step);
+        }
+
+        if (controller.playerplaying.Count > 1)
+        {
+            timer.text = Math.Round(timetokill, 0).ToString();
+            timetokill = timetokill - Time.deltaTime;
+        }
+
+        
+
+    }
+}
