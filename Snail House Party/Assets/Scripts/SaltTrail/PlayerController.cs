6574d3396f76625e4b29e11039f8acc9cad5055c
weizhang05 <chanweizhang05@hotmail.com>
2019-01-26 18:29:31 +0800

    SaltTrail base code

--- Snail House Party/Assets/Scripts/SaltTrail/PlayerController.cs
+++ Snail House Party/Assets/Scripts/SaltTrail/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Controller : MonoBehaviour
 {
     public float speed;
+    public int playerid;
     private Rigidbody2D rb2d;
 
     // Start is called before the first frame update
@@ -16,11 +17,49 @@ public class Controller : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        float moveHorizontal = Input.GetAxisRaw("Horizontal");
-        float moveVertical = Input.GetAxisRaw("Vertical");
+        if (playerid == 1)
+        {
+            float moveHorizontal = Input.GetAxisRaw("Horizontal");
+            float moveVertical = Input.GetAxisRaw("Vertical");
 
-        Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0);
-        rb2d.MovePosition(new Vector2((transform.position.x + movement.x * speed * Time.fixedDeltaTime),
-                            (transform.position.y + movement.y * speed * Time.fixedDeltaTime)));
+            Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0);
+            rb2d.MovePosition(new Vector2((transform.position.x + movement.x * speed * Time.fixedDeltaTime),
+                                (transform.position.y + movement.y * speed * Time.fixedDeltaTime)));
+        }
+
+        if (playerid == 2)
+        {
+            float moveHorizontal = Input.GetAxisRaw("Horizontal2");
+            float moveVertical = Input.GetAxisRaw("Vertical2");
+
+            Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0);
+            rb2d.MovePosition(new Vector2((transform.position.x + movement.x * speed * Time.fixedDeltaTime),
+                                (transform.position.y + movement.y * speed * Time.fixedDeltaTime)));
+        }
+
+        if (playerid == 3)
+        {
+            float moveHorizontal = Input.GetAxisRaw("Horizontal3");
+            float moveVertical = Input.GetAxisRaw("Vertical3");
+
+            Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0);
+            rb2d.MovePosition(new Vector2((transform.position.x + movement.x * speed * Time.fixedDeltaTime),
+                                (transform.position.y + movement.y * speed * Time.fixedDeltaTime)));
+        }
+
+        if (playerid == 4)
+        {
+            float moveHorizontal = Input.GetAxisRaw("Horizontal4");
+            float moveVertical = Input.GetAxisRaw("Vertical4");
+
+            Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0);
+            rb2d.MovePosition(new Vector2((transform.position.x + movement.x * speed * Time.fixedDeltaTime),
+                                (transform.position.y + movement.y * speed * Time.fixedDeltaTime)));
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
     }
 }
