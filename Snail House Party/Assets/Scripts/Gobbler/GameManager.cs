7bae3a3c0b37b86d8405281ec88b4a4a9c05162c
RyouMisakl <33336762+RyouMisakl@users.noreply.github.com>
2019-01-26 10:47:28 +0800

    test

---
+++ Snail House Party/Assets/Scripts/Gobbler/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gobbler
+{
+    public class GameManager : MonoBehaviour
+    {
+        public static GameManager Instance
+        {
+            get; private set;
+        }
+
+        void Awake ()
+        {
+            Instance = this;
+        }
+
+        // Update is called once per frame
+        void Update ()
+        {
+
+        }
+    }
+}
